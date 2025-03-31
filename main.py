93d65497bc1c19bcf38a23be4b2ff2bc840f1e61
Hanting Jin <jinhanting@gmail.com>
2025-03-31 20:14:40 +1100

    give the access to all table s in the database

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
-from langchain.agents import create_openai_functions_agent, AgentExecutor  # 更新导入
+from langchain.agents import create_openai_functions_agent, AgentExecutor 
+from langchain.schema import SystemMessage
+
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_table
 load_dotenv()
 
 chat = ChatOpenAI(temperature=0)
 
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content="You are an AI that has access to a SQlite database."),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad") 
     ]
 )
 
 tools = [run_query_tool]
+tables = list_table()
 
-# 使用新的创建方法
+print(tables)
+import pydantic
+print(pydantic.__version__)
+
+
 agent = create_openai_functions_agent(
     llm=chat,
     prompt=prompt,
@@ -30,5 +38,4 @@ agent_executer = AgentExecutor(
     tools=tools,
 )
 
-# 使用 invoke 替代 run
 agent_executer.invoke({"input": "How many users are in the database?"})
--- tools/sql.py
+++ tools/sql.py
@@ -3,10 +3,19 @@ from langchain.tools import Tool
 
 dataBaseConnection = sqlite3.connect("db.sqlite")
 
+def list_table():
+      c = dataBaseConnection.cursor()
+      c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+      rows = c.fetchall()
+      return rows
+
 def ruin_sqlite_query(query):
     c = dataBaseConnection.cursor()
-    c.execute(query)
-    return c.fetchall()
+    try:
+        c.execute(query)
+        return c.fetchall()
+    except sqlite3.OperationalError as error:
+        return f"An error occurred: {str(error)}"
 
 run_query_tool = Tool.from_function(
     name="run_sqlite_query",
